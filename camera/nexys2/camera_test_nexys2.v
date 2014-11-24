96c015c0be9491a7f170096d426beb28d6a4ab30
Pauline Varley <paulinev@mit.edu>
2014-11-24 14:31:41 -0500

    More bitfile testing for Nexys2

--- camera/nexys2/camera_test_nexys2.v
+++ camera/nexys2/camera_test_nexys2.v
@@ -33,17 +33,17 @@ module camera_test_nexys2(
 );
 
 	// Generic assigns
-	assign JC = 0;
+	assign JC[6:0] = 7'b0;
 	//assign vgaRed = 0;
 	//assign vgaGreen = 0;
 	//assign vgaBlue = 0;
 	//assign Hsync = 0;
 	//assign Vsync = 0;
-	assign sdoid = 0;
+	//assign sdoid = 1;
+	//assign sioc = 1;
 	
 	// Clock dividers
-	wire clock_25mhz_unbuf, clock_25mhz; // actually 10mhz because i am a terrible persion
-	
+	wire clock_25mhz_unbuf, clock_25mhz; 	
 		DCM vgaclk(.CLKIN(clk),.CLKFX(clock_25mhz_unbuf));
 		// synthesis attribute CLKFX_DIVIDE of vgaclk is 4
 		// synthesis attribute CLKFX_MULTIPLY of vgaclk is 2
@@ -51,18 +51,18 @@ module camera_test_nexys2(
 		// synthesis attribute CLKIN_PERIOD of vgaclk is 20
 	   BUFG vgaclk2(.O(clock_25mhz),.I(clock_25mhz_unbuf));
 	   
-	assign xclk = clock_25mhz;
-	   
-	wire clock_100khz_unbuf, clock_100khz; // not slow enough, don't want to worry about it now
-	   DCM sccbclk(.CLKIN(clk),.CLKFX(clock_100khz_unbuf));
-		// synthesis attribute CLKFX_DIVIDE of sccbclk is 25
+	wire clock_10mhz_unbuf, clock_10mhz;
+	DCM sccbclk(.CLKIN(clk),.CLKFX(clock_10mhz_unbuf));
+		// synthesis attribute CLKFX_DIVIDE of sccbclk is 10
 		// synthesis attribute CLKFX_MULTIPLY of sccbclk is 2
 		// synthesis attribute CLK_FEEDBACK of sccbclk is NONE
 		// synthesis attribute CLKIN_PERIOD of sccbclk is 20
-		// synthesis attribute PHASE_SHIFT of sccbclk is 90
-	   BUFG sccbclk2(.O(clock_100khz),.I(clock_100khz_unbuf));
+		// synthesis attribute PHASE_SHIFT of sccbclk is 0
+		// synthesis attribute CLKOUT_PHASE_SHIFT of sccbclk is NONE
+	   BUFG sccbclk2(.O(clock_10mhz),.I(clock_10mhz_unbuf));
 	   
-	assign sccb_clk = clock_100khz;
+	assign sccb_clk = clock_10mhz;
+	assign xclk = clock_10mhz;
 	   
 	// Reset generation
 	wire reset;
@@ -83,47 +83,40 @@ module camera_test_nexys2(
 	segdisplay hexdisp(
 		.clk(clock_25mhz),
 		.enable(0),
-		.value(16'b111111111111111),
+		.value(4'h0abc),
 		.anodes(an),
 		.segments({dp, seg[6:0]}),
 		.tick(tick)
 	);
 		
-	/* Wire up camera inputs/drivers
-	wire sioc;
-	BUFG sioc_buf (.O(sioc), .I(JA[0]));
+	// Wire up camera inputs/drivers
+	//wire sdioc;
+	//BUFG sioc_buf (.O(sioc), .I(JA[0]));
 	
-	wire siod;
-	assign siod = JA[1];
+	//wire sdiod;
+	//assign siod = JA[1];
 	
 	wire vsync_in;
-	assign vsync_in = JA[2];
+	assign vsync_in = JA[3];
 	
 	wire href_in;
-	assign href_in = JA[3];
+	assign href_in = JA[4];
 	
 	wire pclk;
-	BUFG pclk_buf (.O(pclk), .I(JA[4]));
+	assign pclk = JA[5];
 	
-	wire xclk;
-	BUFG clk_buf (.O(JA[5]), .I(clock_25mhz));
+	BUFG clk_buf (.O(JC[7]), .I(xclk));
 	
 	wire [7:0] camera_in;
 	assign camera_in = JB[7:0];
 	
-	wire camera_pwr, camera_gnd;
-	assign camera_power = JA[5];
-	assign JA[5] = 1'b1;
-	
-	assign camera_gnd = JA[6];
-	assign JA[6] = 1'b0;
 	
 // Instantiate camera reader
 	wire [31:0] pixel_out;
 	wire pixel_done, frame_done;
 	camera_read read (
 		.reset(rst), 
-		.clk(clock_25mhz), 
+		.clk(clock_10mhz), 
 		.vsync(vsync_in), 
 		.href(href_in), 
 		.pclk(pclk), 
@@ -137,7 +130,6 @@ module camera_test_nexys2(
 	wire we;
 	wire [18:0] addr;
 	wire [7:0] save_pixel_out;
-	assign led = {we, 7'b1};
 	camera_save frame_buffer(
 		.clk(~pclk),
 		.reset(rst),
@@ -149,7 +141,7 @@ module camera_test_nexys2(
 		.we(we)
 	);
 	
-// Generate VGA signals
+/*// Generate VGA signals
 	VgaRefComp vgadrive(
 		.CLK_25MHz(clock_25mhz),
 		.CLK_40MHz(clk),
@@ -169,7 +161,7 @@ module camera_test_nexys2(
 	assign vgaGreen = bram_out[7:5];
 	assign vgaBlue = bram_out[7:6]; */
 	
-	assign Led = 8'b01010101;
+	assign Led = {we, pixel_done, frame_done, pclk, 4'b0};
 	
 
 endmodule
